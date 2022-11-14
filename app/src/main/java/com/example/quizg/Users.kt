b9dc28034c3ce4da9c36b82bf1a9beadc73419be
Boomika Karuppaiah <k.boomika95@gmail.com>
2022-11-14 13:00:47 -0500

    Added SignUp Flow with Firebase support

--- app/src/main/java/com/example/quizg/Users.kt
+++ app/src/main/java/com/example/quizg/Users.kt
@@ -1,3 +1,3 @@
 package com.example.quizg
 
-data class Users(val username : String)
+data class Users(val firstname : String? = null, val lastname : String? = null, val username : String? = null,val password : String? = null)
